b2dd6f5e3acbc4347f790a9b3d3d3be18e394aca
yeamoon <yeamoon@users.noreply.github.com>
2024-12-31 10:06:55 -0500

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -74,6 +74,6 @@ export default defineConfig({
     webServer: {
         command: "npm run start",
         url: "http://127.0.0.1:3000",
-        reuseExistingServer: !process.env.CI,
+        reuseExistingServer: true,
     },
 });
