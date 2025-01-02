6799e915a87f8c7d3ac12270a7e21d3a02566fba
yeamoon <yeamoon@users.noreply.github.com>
2025-01-01 19:22:30 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -31,15 +31,3 @@ EOF
   }
 }
 
-# Firewall rule to allow HTTP traffic
-resource "google_compute_firewall" "default" {
-  name    = "allow-http"
-  network = "default"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["80"]
-  }
-
-  source_ranges = ["0.0.0.0/0"]
-}
